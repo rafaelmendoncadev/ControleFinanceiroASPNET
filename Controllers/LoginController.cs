e97649b9940e6c156427e1a28c005126f0685c2d
rafaelmendoncadev <recargaoriginal@hotmail.com>
2025-06-24 11:33:57 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFinanceiro.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string userName, string password)
+        {
+            if (userName == "admin" && password == "senha123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = "Usuário ou senha inválidos.";
+            return View();
+        }
+    }
+}
